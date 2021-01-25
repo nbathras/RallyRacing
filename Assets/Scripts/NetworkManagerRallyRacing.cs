46b3d5e7f1e50187a9d0372c785a80b2a5cac55e
Noah Bathras <nbathras@terpmail.umd.edu>
2021-01-24 23:40:25 -0500

    Inital Commit with Mirror and ParrelSync and extended network manager and player and basic race course

---
+++ Assets/Scripts/NetworkManagerRallyRacing.cs
@@ -0,0 +1,38 @@
+﻿using Mirror;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Custom NetworkManager that simply assigns the correct racket positions when
+// spawning players. The built in RoundRobin spawn method wouldn't work after
+// someone reconnects (both players would be on the same side).
+[AddComponentMenu("")]
+public class NetworkManagerRallyRacing : NetworkManager {
+    public List<Transform> playerSpawnPositionList;
+
+    public override void OnServerAddPlayer(NetworkConnection conn) {
+        // add player at correct spawn position
+        Transform spawnPosition = playerSpawnPositionList[numPlayers];
+        GameObject player = Instantiate(playerPrefab, spawnPosition.position, spawnPosition.rotation);
+        NetworkServer.AddPlayerForConnection(conn, player);
+
+        // spawn ball if two players
+        /*
+        if (numPlayers == 2) {
+            ball = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Ball"));
+            NetworkServer.Spawn(ball);
+        }
+        */
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn) {
+        // destroy ball
+        /*
+        if (ball != null) {
+            NetworkServer.Destroy(ball);
+        }
+        */
+
+        // call base functionality (actually destroys the player)
+        base.OnServerDisconnect(conn);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using Mirror;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class PlayerController : NetworkBehaviour {
+    [SerializeField]
+    private List<Sprite> spriteList;
+    private static int numberOfPlayers = 0;
+
+    [SerializeField]
+    private float acceleration;
+    [SerializeField]
+    private float steering;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        sr.enabled = false;
+        numberOfPlayers++;
+    }
+
+    private void Start() {
+        CmdSetPlayerSprite(numberOfPlayers - 1);
+        sr.enabled = true;
+    }
+
+    [Command]
+    public void CmdSetPlayerSprite(int i) {
+        Debug.Log("Sprite1: " + i.ToString());
+        RpcSetPlayerSprite(i);
+    }
+
+    [ClientRpc]
+    public void RpcSetPlayerSprite(int i) {
+        Debug.Log("Sprite2: " + i.ToString());
+        sr.sprite = spriteList[i];
+    }
+
+    private void FixedUpdate() {
+        if (isLocalPlayer) {
+            HandleMovement();
+        }
+    }
+
+    private void HandleMovement() {
+        float h = -Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 speed = transform.up * (v * acceleration);
+        rb.AddForce(speed);
+
+        float direction = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up));
+        if (direction >= 0.0f) {
+            rb.rotation += h * steering * (rb.velocity.magnitude / 5.0f);
+        } else {
+            rb.rotation -= h * steering * (rb.velocity.magnitude / 5.0f);
+        }
+
+        Vector2 forward = new Vector2(0.0f, 0.5f);
+        float steeringRightAngle;
+        if (rb.angularVelocity > 0) {
+            steeringRightAngle = -90;
+        } else {
+            steeringRightAngle = 90;
+        }
+
+        Vector2 rightAngleFromForward = Quaternion.AngleAxis(steeringRightAngle, Vector3.forward) * forward;
+
+        float driftForce = Vector2.Dot(rb.velocity, rb.GetRelativeVector(rightAngleFromForward.normalized));
+
+        Vector2 relativeForce = (rightAngleFromForward.normalized * -1.0f) * (driftForce * 10.0f);
+
+        rb.AddForce(rb.GetRelativeVector(relativeForce));
+    }
+
+    private void OnDestroy() {
+        numberOfPlayers--;
+    }
+}
